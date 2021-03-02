d268ffd532df2bca445790692f6bc0d2192c39b7
kintrix <boldizsar02@gmail.com>
2021-03-02 16:20:05 +0100

    Brushed down everything to a bare minimum

--- source/classes/types.ts
+++ source/classes/types.ts
@@ -1,51 +1,12 @@
 import * as DC from "discord.js";
-import Time from "./time";
 
-export interface LessonData {
-    subj:       string;
-    elective:   boolean;
-}
-
-// Timetable
-export interface Lesson extends LessonData {
-    start:  Time;
-    end:    Time;
-    length: number;
-}
-
-export type TimetableDay = Lesson[];
-
-export interface Timetable {
-    [day: string]:  TimetableDay;
-}
-
-// Students
-
-export interface LessonsAttendants {
-    [lesson: string]: {
-        obligatory?:    string[];
-        elective?:      string[];
-    };
-}
-
-export interface StudentsLessons {
-    [student: string]:  LessonData[];
-}
-
-export interface Students {
-    roster:             string[];
-    lessonsStudents:    LessonsAttendants;
-    studentsLessons:    StudentsLessons;
-}
-
-// Data
+// NOT framework specific
 export interface Data {
     client:         DC.Client;
-    timetable:      Timetable;
-    students:       Students;
     defaultPrefix:  string;
 }
 
+// framework-specific
 export interface CombinedData {
     data:       Data;
     msg:        DC.Message;
