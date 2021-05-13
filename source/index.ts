741822b0026a0be9aa24052c947cba9443b46085
kintrix <boldizsar02@gmail.com>
2021-05-13 18:04:40 +0200

    general structural improvements

--- source/index.ts
+++ source/index.ts
@@ -1,9 +1,12 @@
 import { initBot } from "./_core/bot_core";
 import path from "path";
 
-initBot({
-    defaultPrefix: ".",
-    commandDirs: [
-        path.join(__dirname, "bot_commands")
-    ]
-});
+initBot(
+    {
+        defaultPrefix: ".",
+        commandDirs: [
+            path.join(__dirname, "bot_commands")
+        ]
+    },
+    {}
+);
